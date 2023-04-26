0277fd0da89d8a036a867442272dc4309bfb35fa
VolkovK04 <Volkov_Kirill2004@mail.ru>
2023-04-26 20:58:55 +0300

    update

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-from collections import deque
-
 class TreeNode(object):
     def __init__(self, x):
         self.val = x
@@ -15,14 +13,48 @@ class Codec:
             return "*"
 
     def deserialize(self, data):
-        queue = deque(data.split())
-        return self.deserialize_imp(queue)
+        arr = data.split()
+        return self.deserialize_imp(arr)
 
-    def deserialize_imp(self, queue):
-        val = queue.popleft()
+    def deserialize_imp(self, arr):
+        val = arr.pop(0)
         if val == "*":
             return None
         node = TreeNode(int(val))
-        node.left = self.deserialize_imp(queue)
-        node.right = self.deserialize_imp(queue)
+        node.left = self.deserialize_imp(arr)
+        node.right = self.deserialize_imp(arr)
         return node
+
+
+class Solution:
+    def rightSideView(self, root, height=0, res=None):
+        if res is None:
+            res = []
+        if root:
+            if len(res) == height:
+                res.append(root.val)
+            height += 1
+            self.rightSideView(root.right, height, res)
+            self.rightSideView(root.left, height, res)
+        return res
+
+
+class Solution1:
+    def isValidBST(self, root, min_val = float('-inf'), max_val = float('inf')):
+        if root is None:
+            return True
+        if root.val <= min_val or root.val >= max_val:
+            return False
+        return self.isValidBST(root.left, min_val, root.val) and self.isValidBST(root.right, root.val, max_val)
+
+class Solution2:
+    def trimBST(self, root, low, high):
+        if root is None:
+            return
+        if root.val < low:
+            return self.trimBST(root.right, low, high)
+        elif high < root.val:
+            return self.trimBST(root.left, low, high)
+        root.left = self.trimBST(root.left, low, high)
+        root.right = self.trimBST(root.right, low, high)
+        return root
